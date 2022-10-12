b15b496957ebac5a2f476ec6170b11c55b3b657b
JulietteGutierrez <juli.gutierrez@live.com.ar>
2022-10-12 15:20:03 -0300

    cambio de fotos en la tienda

--- tienda-online/productos.js
+++ tienda-online/productos.js
@@ -16,15 +16,14 @@ const CAFETERAS = 'Cafeteras'
 const CAPSULAS = 'Cápsulas'
 const SOPORTES = 'Soportes'
 const PACKS = 'Packs'
-const TAZAS = 'Tazas'
 
 const descripcionDefault = 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'
 
 export default [
       new Producto('Soporte Individual', 500, [SOPORTES], 'soporte.jpeg', descripcionDefault)
     , new Producto('Pack Strainer x3 ', 1600, [PACKS], 'pack.jpeg', descripcionDefault)
-    , new Producto('Pack Strainer x6', 3500, [PACKS], 'pack.jpeg', descripcionDefault)
-    , new Producto('Taza Strainer', 1600, [TAZAS], 'taza.jpeg', descripcionDefault)
+    , new Producto('Pack Strainer x6', 3500, [PACKS], 'pack2.jpg', descripcionDefault)
+    , new Producto('Taza Strainer', 1600, [MERCHANDISING], 'taza2.jpeg', descripcionDefault)
     , new Producto('Cafetera Dolce Gusto', 24000,  [CAFETERAS], 'cafetera-dolce.webp', descripcionDefault)
     , new Producto('Cafetera Oyster', 39000,  [CAFETERAS], 'cafetera-oster.webp', descripcionDefault)
     , new Producto('Cápsulas Dolce Gusto Chococino', 2700, [CAPSULAS], 'capsulas-chococino.webp', descripcionDefault)
@@ -34,5 +33,5 @@ export default [
     , new Producto('Remera Astronauta Luna', 2700, [MERCHANDISING], 'remera-astronauta-luna.webp', descripcionDefault)
     , new Producto('Remera Astronauta Taza', 2700, [MERCHANDISING], 'remera-astronauta-cafe.webp', descripcionDefault)
     , new Producto('Remera Casa Cafetera', 2700, [MERCHANDISING], 'remera-casa.webp', descripcionDefault)
-
+    
 ]    
